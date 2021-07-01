dfffb0d91a405ef75155917aefca6e89b82829e7
Student <student@lan.itstep.kiev.ua>
2021-07-01 22:00:29 +0300

    Оптимизация проверки на число

--- 2. Lesson/file01.py
+++ 2. Lesson/file01.py
@@ -1,17 +1,37 @@
 #
 # Ожидаем ввод первого числа и проверяем, что введено именно число
+
 dig1 = input("Первое число:")
 print(dig1.isdigit())
 if True:
      dig1 = int(dig1)
+else:
+    print('Это не число')
+    exit()
 
-
-# Ожидаем выбор математической операции. Проверяем, что выбрали одну из предложенных....
+# Ожидаем выбор математической операции. Проверяем, что выбрали одну из предложенных... И ожидаем ввод второго числа
 operations=str(input("Выберите одну из операций: (+  -  *  /)\n"))
 if operations == "+" or operations == "-" or operations == "*" or operations == "/":
-#
-#... И ожидаем ввод второго числа
+     dig2 = input("Второе число:")
+     print(dig2.isdigit())
+     if True:
+          dig2 = int(dig2)
+     else:
+          print('Это не число')
+          exit()
 
+else: print("Неверный выбор"), exit()
+
+if operations == "+":
+    print (dig1, "+",dig2, "=", dig1+dig2)
+elif operations == "-":
+    print (dig1, "-",dig2, "=", dig1-dig2)
+elif operations=="*":
+    print (dig1, "*",dig2, "=", dig1*dig2)
+elif operations=="/":
+    print (dig1, "/",dig2, "=", dig1/dig2)
+
+else: print("Неверный выбор"), exit()
 
 # if dig1.isdigit() == True:
 # else:

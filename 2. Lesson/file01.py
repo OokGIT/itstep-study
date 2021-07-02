a776fc9f40614535979a17793b4273d1317d3a1b
point48 <point48@node48.feerie.lc>
2021-07-02 15:38:15 +0300

    Попарвил синтаксис

--- 2. Lesson/file01.py
+++ 2. Lesson/file01.py
@@ -1,21 +1,21 @@
 # Ожидаем выбор математической операции. Проверяем, что выбрали одну из предложенных.
-operations=str(input("Выберите одну из математических операций: (+  -  *  /)\n"))
+operations = str(input("Выберите одну из математических операций: (+  -  *  /)\n"))
 if operations != "+" and operations != "-" and operations != "*" and operations != "/":
-    print ("Выбрана неверная математическая операция")
+    print("Выбрана неверная математическая операция")
     exit()
 
 # Ожидаем ввод первого числа и проверяем, что введено именно число
 dig1 = input("Первое число:")
-if dig1.isdigit() == True:
+if dig1.isdigit() is True:
     dig1 = int(dig1)
 #    print (dig1)
 else:
-    print ('введено не число')
+    print('введено не число')
     exit()
 
 # Ожидаем ввод второго числа и проверяем, что введено именно число
 dig2 = input("Второе число:")
-if dig2.isdigit() == True:
+if dig2.isdigit() is True:
     dig2 = int(dig2)
 #    print (dig2)
 else:
@@ -23,11 +23,12 @@ else:
     exit()
 
 # Проводим математические операции с двумя числами
+print("Ваш результат:")
 if operations == "+":
-    print (dig1, "+",dig2, "=", dig1+dig2)
+    print(dig1, "+", dig2, "=", dig1+dig2)
 elif operations == "-":
-    print (dig1, "-",dig2, "=", dig1-dig2)
-elif operations=="*":
-    print (dig1, "*",dig2, "=", dig1*dig2)
-elif operations=="/":
-    print (dig1, "/",dig2, "=", dig1/dig2)
+    print(dig1, "-", dig2, "=", dig1-dig2)
+elif operations == "*":
+    print(dig1, "*", dig2, "=", dig1*dig2)
+elif operations == "/":
+    print(dig1, "/", dig2, "=", dig1/dig2)

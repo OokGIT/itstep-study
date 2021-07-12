5122c5ff81f8a43c496e4bca3eb667f10d5555be
point48 <point48@node48.feerie.lc>
2021-07-12 12:05:06 +0300

    Оно уже как-то работает! )) Есть цикл, есть проверки, exceptions и функции вынесены в отдельный файл

---
+++ 03Calculator/_functions.py
@@ -0,0 +1,36 @@
+# Обработка ввода дополнительных команд
+def externalCommands():
+    result = input("ждем команду exit или clear")
+    if result.lower() == "exit":
+        result = "exit"
+    elif result.lower() == "clean":
+        result = "clean"
+
+# ожидание ввода числа, проверка на число и отлов ошибок
+def numeric_inp1():
+    try:
+        result = float(input('Введите число... '))
+        return result
+    except ValueError as err:
+        print("Это не похоже на число...", err)
+    except:
+        return None
+
+# Возврат выбора математической операции
+def calc_operator():
+    result_calc = input("введите +-*/")
+    if result_calc != "+" and result_calc != "-" and result_calc != "*" and result_calc != "/" and result_calc != "exit" and result_calc != "clean":
+        print("не тот знак")
+        return False
+    elif result_calc == "+":
+        return "add"
+    elif result_calc == "-":
+        return "minus"
+    elif result_calc == "*":
+        return "multi"
+    elif result_calc == "/":
+        return "div"
+    elif result_calc == "exit":
+        return "exit"
+    elif result_calc == "clean":
+        return "clean"

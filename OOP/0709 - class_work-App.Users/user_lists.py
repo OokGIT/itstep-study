5fbc05e8fa40e256fcfcbd30a7ec3f687eadc309
ookgit <Mf2807Ka>
2021-09-10 14:02:48 +0300

    Magic methods task update

--- OOP/0709 - class_work-App.Users/user_lists.py
+++ OOP/0709 - class_work-App.Users/user_lists.py
@@ -1,7 +1,7 @@
 import random
 
 names = ['Иван', 'Петр', 'Василий', 'Геннадий']
-surnames = ['Данилов', 'Ефремов', 'Гончаренко', 'Мартыненко', 'Стругацкий']
+lastnames = ['Данилов', 'Ефремов', 'Гончаренко', 'Мартыненко', 'Стругацкий']
 cities = ['Киев', 'Одесса', 'Ереван', 'Херсон']
 
 print("Длина стартового списка:", len(names))
@@ -11,12 +11,13 @@ users_db = dict()
 
 for i in range(len(names)):
     users_db_item['name'] = random.choice(names)
-    users_db_item['surname'] = random.choice(surnames)
+    users_db_item['lastname'] = random.choice(lastnames)
     users_db_item['age'] = random.randrange(18, 56)
     users_db_item['city'] = random.choice(cities)
     users_db_item['user_id'] = hash(random.randrange(1, 52595))
     users_db[i] = users_db_item
-    print(i)
-    print(users_db[i])
+    # print(i)
+    # print(users_db[i])
+print(users_db)
 
 

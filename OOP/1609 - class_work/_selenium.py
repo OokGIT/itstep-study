b4e2353e53676d0a4efa9a3f90ca7bda8cde9834
ookgit <Mf2807Ka>
2021-09-16 21:31:30 +0300

    Selenium - получили успешный вход в просмотр заявок на CRM-ке Феерии

---
+++ OOP/1609 - class_work/_selenium.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+# import base64
+# from selenium.webdriver.common.keys import Keys
+# import os
+
+my_pass = 'Riper2807'
+
+driver = webdriver.Chrome(executable_path="./chromedriver")
+
+# Токен выдается в CRM с привязкой к IP.
+# Если IP "левый", то после логина может вылезти капча.
+driver.get('https://online.feeria.ua/cl_refer?oauth_token=2c1c982f45164fddba72cb407b3b9317')
+
+element = driver.find_element_by_id('login')
+element.send_keys('aeroua')
+element = driver.find_element_by_id('passwd')
+element.send_keys(my_pass)
+button = driver.find_element_by_class_name('button')
+button.click()
+

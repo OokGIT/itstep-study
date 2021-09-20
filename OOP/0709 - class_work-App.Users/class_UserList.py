4f9b3043213a6eeb8b30fbe4bf1accdb056f16f0
ookgit <Mf2807Ka>
2021-09-20 18:16:33 +0300

    Users list App. All done except Excel export.

--- OOP/0709 - class_work-App.Users/class_UserList.py
+++ OOP/0709 - class_work-App.Users/class_UserList.py
@@ -1,4 +1,8 @@
 import random
+
+import xlsxwriter
+from xlsxwriter import Workbook
+
 users_list = [{'user_id': '18456', 'name': 'Артем', 'lastname': 'Данилов',
                'age': '44', 'city': 'Херсон'},
               {'user_id': '40385', 'name': 'Иван', 'lastname': 'Гончаренко',
@@ -20,7 +24,6 @@ class UserList:
                 print(key, user_item[key])
             print('------------------')
 
-
     def add_user(self):
         new_user = dict()
         new_user['user_id'] = str(hash(random.randrange(1, 52595)))
@@ -32,7 +35,8 @@ class UserList:
         self.get_users()
 
     def change_user(self):
-        new_item = int(input('Выберите по какому полю ищем пользователя'
+        new_item_search = ''
+        new_item = int(input('Выберите значение какого поля мы будем менять'
                              '(цифра от 1 до 5): 1-ID, 2-Имя, 3-Фамилия,'
                              ' 4-Возраст, 5-Город... '))
         if new_item == 1:
@@ -55,7 +59,7 @@ class UserList:
                 if value == search_value:
                     print('По условию найдена запись:\n', self.user_item)
                     if input('Вносим изменения в данные этого пользователя?'
-                             ' Y / N').lower().strip() == 'y':
+                             ' y / n') == 'y':
                         new_value = input('Введите новое значение')
                         self.user_item[key] = new_value
                         print('Новое значение записи:', self.user_item)
@@ -63,8 +67,10 @@ class UserList:
                         print('Оставляем запись без изменения')
 
     def del_user(self):
-        new_item = int(input('Выберите по какому полю ищем пользователя(цифра от 1 до'
-                             ' 5): 1-ID, 2-Имя, 3-Фамилия, 4-Возраст, 5-Город... '))
+        new_item_search = ''
+        new_item = int(input('Выберите по какому полю ищем пользователя'
+                             '(цифра от 1 до 5): 1-ID, 2-Имя, 3-Фамилия,'
+                             ' 4-Возраст, 5-Город... '))
         if new_item == 1:
             new_item_search = 'user_id'
         elif new_item == 2:
@@ -84,7 +90,7 @@ class UserList:
             for key, value in self.user_item.items():
                 if value == search_value:
                     print('По условию найдена запись:\n', self.user_item)
-                    if input('Удаляем данного пользователя ? Y / N') == ('y' or 'Y'):
+                    if input('Удаляем данного пользователя ? y / n') == 'y':
                         # users_list = users_list.remove(user_item)
                         del self.my_list[i]
                         print('New users list:', self.my_list)
@@ -92,8 +98,10 @@ class UserList:
                         print('Оставляем запись без изменения')
 
     def search_user(self):
-        new_item = int(input('Выберите по какому полю ищем пользователя(цифра от 1 до'
-                             ' 5): 1-ID, 2-Имя, 3-Фамилия, 4-Возраст, 5-Город... '))
+        new_item_search = ''
+        new_item = int(input('Выберите по какому полю ищем пользователя'
+                             '(цифра от 1 до 5): 1-ID, 2-Имя, 3-Фамилия,'
+                             ' 4-Возраст, 5-Город... '))
         if new_item == 1:
             new_item_search = 'user_id'
         elif new_item == 2:
@@ -113,3 +121,19 @@ class UserList:
             for key, value in user_item.items():
                 if value == search_value:
                     print('По условию найдена запись:\n', user_item)
+
+    def write_to_xlsx(self):
+        with xlsxwriter.Workbook('users.xlsx') as workbook:
+            worksheet = workbook.add_worksheet()
+            first_col = 0
+            user_item = self.my_list[0]
+            print(user_item)
+            worksheet.write(0, 0, 'ID')
+            worksheet.write(0, 1, 'Name')
+            worksheet.write(0, 2, 'Lastname')
+            worksheet.write(0, 3, 'Age')
+            worksheet.write(0, 4, 'City')
+            for i in range(len(self.my_list)):
+                user_item = self.my_list[i]
+                for i, key, value in enumerate(user_item, start=1):
+                    worksheet.write(i, 1, user_item[key])
--- OOP/0709 - class_work-App.Users/main.py
+++ OOP/0709 - class_work-App.Users/main.py
@@ -1,129 +1,31 @@
 from class_UserList import UserList
-# import random
 
 
 My_App = UserList()
-
-
-def get_choice():
+menu_item = 0
+# My_App.change_user()
+while True:
     print('Here we can:\n', '- 1 Get users list\n', '- 2 Add new user\n',
           '- 3 Change user\n', '- 4 Delete user\n', '- 5 Search user\n',
           '- 6 Put users list to Excel')
-    while True:
-        get_number = int(input('Chose an action from 1 to 6... '))
-        if get_number in range(1, 6):
-            print('Ok,', get_number, 'selected')
-            return get_number
-
-
-if get_choice() == 1:
-    print('---------- User List ----------')
-    My_App.get_users()
-elif get_choice() == 2:
-    My_App.add_user()
-elif get_choice() == 3:
-    My_App.change_user()
-elif get_choice() == 4:
-    My_App.del_user()
-elif get_choice() == 5:
-    My_App.search_user()
-
-# Print users list (task 1)
-# for i in range(len(users_list)):
-#     user_item = users_list[i]
-#     for key, value in user_item.items():
-#         print(key, user_item[key])
-#     print('------------------')
-
-# Add user to list (task 2)
-# using list.append
-# new_user = dict()
-# new_user['user_id'] = str(hash(random.randrange(1, 52595)))
-# new_user['name'] = input('Имя пользователя:')
-# new_user['lastname'] = input('Фамилия пользователя:')
-# new_user['age'] = input('Возраст:')
-# new_user['city'] = input('Город:')
-# users_list.append(dict(new_user))
-# print(users_list)
-
-# Change user (task 3)
-# new_item = int(input('Выберите по какому полю ищем пользователя(цифра от 1 до'
-#                      ' 5): 1-ID, 2-Имя, 3-Фамилия, 4-Возраст, 5-Город... '))
-# if new_item == 1:
-#     new_item_search = 'user_id'
-# elif new_item == 2:
-#     new_item_search = 'name'
-# elif new_item == 3:
-#     new_item_search = 'lastname'
-# elif new_item == 4:
-#     new_item_search = 'age'
-# elif new_item == 5:
-#     new_item_search = 'city'
-# else:
-#     print('Неверный выбор')
-# search_value = input(f'Введите искомое значение для'
-#                      f' выбранного поля {new_item_search}... ')
-# for i in range(len(users_list)):
-#     user_item = users_list[i]
-#     for key, value in user_item.items():
-#         if value == search_value:
-#             print('По условию найдена запись:\n', user_item)
-#             if input('Вносим изменения в данные этого пользователя? Y / N') == ('y' or 'Y'):
-#                 new_value = input('Введите новое значение')
-#                 user_item[key] = new_value
-#                 print('Новое значение записи:', user_item)
-#             else:
-#                 print('Оставляем запись без изменения')
-
-# Delete user (task 4)
-# new_item = int(input('Выберите по какому полю ищем пользователя(цифра от 1 до'
-#                      ' 5): 1-ID, 2-Имя, 3-Фамилия, 4-Возраст, 5-Город... '))
-# if new_item == 1:
-#     new_item_search = 'user_id'
-# elif new_item == 2:
-#     new_item_search = 'name'
-# elif new_item == 3:
-#     new_item_search = 'lastname'
-# elif new_item == 4:
-#     new_item_search = 'age'
-# elif new_item == 5:
-#     new_item_search = 'city'
-# else:
-#     print('Неверный выбор')
-# search_value = input(f'Введите искомое значение для'
-#                      f' выбранного поля {new_item_search}... ')
-# for i in range(len(users_list)):
-#     user_item = users_list[i]
-#     for key, value in user_item.items():
-#         if value == search_value:
-#             print('По условию найдена запись:\n', user_item)
-#             if input('Удаляем данного пользователя ? Y / N') == ('y' or 'Y'):
-#                 # users_list = users_list.remove(user_item)
-#                 del users_list[i]
-#                 print('New users list:', users_list)
-#             else:
-#                 print('Оставляем запись без изменения')
-
-
-# Search user (task 5)
-# new_item = int(input('Выберите по какому полю ищем пользователя(цифра от 1 до'
-#                      ' 5): 1-ID, 2-Имя, 3-Фамилия, 4-Возраст, 5-Город... '))
-# if new_item == 1:
-#     new_item_search = 'user_id'
-# elif new_item == 2:
-#     new_item_search = 'name'
-# elif new_item == 3:
-#     new_item_search = 'lastname'
-# elif new_item == 4:
-#     new_item_search = 'age'
-# elif new_item == 5:
-#     new_item_search = 'city'
-# else:
-#     print('Неверный выбор')
-# search_value = input(f'Введите искомое значение для'
-#                      f' выбранного поля {new_item_search}... ')
-# for i in range(len(users_list)):
-#     user_item = users_list[i]
-#     for key, value in user_item.items():
-#         if value == search_value:
-#             print('По условию найдена запись:\n', user_item)
+    menu_item = int(input('Chose an action from 1 to 6... '))
+    if menu_item in range(1, 6):
+        print('Ok,', menu_item, 'selected')
+    if menu_item == 1:
+        print('---------- User List ----------')
+        My_App.get_users()
+    elif menu_item == 2:
+        print('---------- Add a new user ----------')
+        My_App.add_user()
+    elif menu_item == 3:
+        print('---------- Change user ----------')
+        My_App.change_user()
+    elif menu_item == 4:
+        print('---------- Delete user ----------')
+        My_App.del_user()
+    elif menu_item == 5:
+        print('---------- Search user ----------')
+        My_App.search_user()
+    elif menu_item == 6:
+        print('---------- Write to xlsx ----------')
+        My_App.write_to_xlsx()

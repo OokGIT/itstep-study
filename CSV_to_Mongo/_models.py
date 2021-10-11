d0ee519bffd3b4407b38b1606218d3f11372312d
point48 <aeroflot.ua@gmail.com>
2021-10-11 15:27:08 +0300

    Removed url as Primary Key. Added validation by unique string on parsing step

--- CSV_to_Mongo/_models.py
+++ CSV_to_Mongo/_models.py
@@ -1,13 +1,14 @@
 from mongoengine import *
 connect(host="mongodb://127.0.0.1:27017/csv_to_mongo")
 
+
 class WriteToDb(Document):
-    url = StringField(required=True, primary_key=True)
-    intern_id = StringField(max_length=12)
+    internal_id = StringField(max_length=12)
     title = StringField()
     description = StringField()
-    published_at_utc = DateTimeField()
-
+    url = StringField(required=True)
+    published_utc = DateTimeField()
+    site = StringField()
 
     def __str__(self):
         return self.url
--- CSV_to_Mongo/read_csv.py
+++ CSV_to_Mongo/read_csv.py
@@ -1,25 +1,21 @@
 import csv
-import re
-from dateutil.parser import parse
+import datetime
 from _models import WriteToDb
 
-# def parse_date_convert(date, fmt=None):
-#     if fmt is None:
-#         fmt = '%Y-%m-%d %H:%M:%S' # Defaults to : 2022-08-31 07:47:30
-#     get_date_obj = parse(str(date))
-#     return str(get_date_obj.strftime(fmt))
+CSV_FILE = 'inbound.csv'
+CSV_ENCODING = 'utf-8-sig'
 
 
-
-
-with open('inbound.csv', 'r', encoding='utf-8-sig') as f:
+with open(CSV_FILE, 'r', encoding=CSV_ENCODING) as f:
     read_csv = csv.reader(f)
     headers_list = next(read_csv)
-    # print(headers_list)
     if headers_list is not None:
         for line in read_csv:
-            WriteToDb(url=line[4], intern_id=line[0], title=re.sub('\W+', ' ',line[2]), description=line[3], published_at_utc=line[5]).save()
-            # item_date = parse_date_convert(line[5])
-            # print(item_date)
-
-
+            if WriteToDb.objects.filter(url=line[4]):
+                print('Item already in DB. Duplicates will not be written')
+            elif not line[4].startswith('http'):
+                print('Seems there is not http/https address found. String will not be written to DB')
+                continue
+            WriteToDb(internal_id=line[0], title=line[2], description=line[3], url=line[4],
+                      published_utc=datetime.datetime.strptime(line[5], '%Y-%m-%d %H:%M:%S%z'),
+                      site=line[11]).save()

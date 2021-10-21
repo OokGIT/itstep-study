f25271bc32f994171dc73f2910e6b8ea9ca43412
OokGIT <aeroflot.ua@gmail.com>
2021-10-21 21:10:52 +0300

    some classwork on postgress connection

--- Postgres/classwork/01.py
+++ Postgres/classwork/01.py
@@ -4,7 +4,15 @@ conn = psycopg2.connect(dbname='test01', user='postgres',
                         password='postgres', host='localhost')
 cur = conn.cursor()
 
-cur.execute('SELECT * FROM user_list LIMIT 10')
+create_users = '''CREATE TABLE users
+(   
+    user_id   serial PRIMARY KEY,
+    username "char"
+);
+'''
+
+cur.execute(create_users)
+conn.commit()
 records = cur.fetchall()
 print(records)
 for row in records:

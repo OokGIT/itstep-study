c3f5be46907ce3b4d53c6b77323d4c98b708a20f
point48 <aeroflot.ua@gmail.com>
2021-10-22 15:03:29 +0300

    Uses all commands from homework (update, select where, select *...)

--- Postgres/homework/01.py
+++ Postgres/homework/01.py
@@ -4,7 +4,13 @@ import project_func
 #
 # Создаем таблицы, на вход функции отправляем имя таблицы. Поля предопределены заранее
 # project_func.create_table('phones06')
-# project_func.create_table('phones07')
+# project_func.create_table('phones08')
 
 # Заполняем таблицу данными
-project_func.fill_data('phones07', 2, 'Samsung', 13456)
+# project_func.fill_data('phones08', 'Mi9', 8650)
+
+# Запишем в БД новую цену
+# project_func.update_price('phones08', 2, 555555)
+
+# Найдем телефоны дешевле указанной цены
+project_func.search_min_price('phones08', 9000)
--- Postgres/homework/project_func.py
+++ Postgres/homework/project_func.py
@@ -15,7 +15,7 @@ def get_db_info():
 
 def create_table(tb_name):
     create_table_query = f'''CREATE TABLE {tb_name}
-(ID INT PRIMARY KEY NOT NULL,
+(ID SERIAL PRIMARY KEY,
 MODEL   TEXT    NOT NULL,
 PRICE   REAL);'''
     cursor.execute(create_table_query)
@@ -23,12 +23,28 @@ PRICE   REAL);'''
     print("Создана таблица", tb_name)
 
 
-def fill_data(table_name, mobile_id, mobile_model, mobile_price):
-    insert_query = f''' INSERT INTO {table_name} (ID, MODEL, PRICE) VALUES ({mobile_id}, '{mobile_model}', {mobile_price})'''
-    # insert_query = ''' INSERT INTO phones07 (ID, MODEL, PRICE) VALUES (1, 'huaweii', 10000)'''
+def fill_data(table_name, mobile_model, mobile_price):
+    insert_query = f''' INSERT INTO {table_name} (MODEL, PRICE) VALUES ('{mobile_model}', {mobile_price})'''
     cursor.execute(insert_query)
     conn.commit()
     print("Запись создана")
     cursor.execute(f"SELECT * from {table_name}")
     record = cursor.fetchall()
     print("Результат:", record)
+
+
+def update_price(table_name, mobile_id, new_price):
+    update_query = f'''UPDATE {table_name} SET PRICE = {new_price} WHERE ID = {mobile_id}'''
+    cursor.execute(update_query)
+    conn.commit()
+    print("Цена обновлена")
+    cursor.execute(f"SELECT * from {table_name}")
+    record = cursor.fetchall()
+    print("Результат:", record)
+
+
+def search_min_price(table_name, search_price):
+    search_query = f''' SELECT * FROM {table_name} WHERE PRICE < {search_price}'''
+    cursor.execute(search_query)
+    record = cursor.fetchall()
+    print(record)

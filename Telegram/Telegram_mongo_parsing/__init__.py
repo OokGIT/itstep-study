4738dbef9519172e3daef88db76e6fc57981e861
point48 <aeroflot.ua@gmail.com>
2021-11-03 14:40:51 +0200

    Save log to file

--- Telegram/Telegram_mongo_parsing/__init__.py
+++ Telegram/Telegram_mongo_parsing/__init__.py
@@ -2,36 +2,35 @@ import telebot
 import pymongo
 from datetime import datetime
 import conf
+import wiki_search
 
 bot = telebot.TeleBot(conf.BOT_API, parse_mode=None)
 client = pymongo.MongoClient('localhost', 27017)
 db = client['search_bot']
 
 
-def check_and_add_user(message):
-    if db.users.find_one({'user_id': message.from_user.id}) is None:
-        new_user = {
-            'first_name': message.from_user.first_name,
-            'last_name': message.from_user.last_name,
-            'user_id': message.from_user.id,
-            'date': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-            'state': 'старт'
-        }
-        db.users.insert_one(new_user)
-    return
+# def check_and_add_user(message):
+#     if db.users.find_one({'user_id': message.from_user.id}) is None:
+#         new_user = {
+#             'first_name': message.from_user.first_name,
+#             'last_name': message.from_user.last_name,
+#             'user_id': message.from_user.id,
+#             'date': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+#             'state': 'старт'
+#         }
+#         db.users.insert_one(new_user)
+#     return
 
 
-def search_message_in_db(message):
-    state = 'None'
-    if db.messages.find_one({'search_message': message.text}) is None:
-        search_message = {
+def store_message_in_db(message):
+    store_message = {
             'user_id': message.from_user.id,
             'message_time': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-            'user_message': message.text
+            'user_message': message.text,
+            'state': None
         }
-        state = 'Это новый запрос'
-        db.messages.insert_one(search_message)
-    return state
+    db.messages.insert_one(store_message)
+    return None
 
 
 def get_current_state(user_id):
@@ -45,26 +44,28 @@ def set_state(user_id, state_value):
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    check_and_add_user(message)
-    bot.reply_to(message, "Howdy, how are you doing?")
+     bot.reply_to(message, "Просто введите строку и я найду для вас информацию на Википедии")
 
 
-@bot.message_handler(commands=['info'])
-def send_welcome(message):
-    u_id = message.from_user.id
-    u_name = message.from_user.first_name
-    u_lastname = message.from_user.last_name
-    u_username = message.from_user.username
-    print(u_id, u_name, u_lastname, u_username)
-    bot.reply_to(message, f"Welcome, user{u_id}, {u_name}, {u_lastname}, {u_username}")
+# @bot.message_handler(commands=['info'])
+# def send_welcome(message):
+#     u_id = message.from_user.id
+#     u_name = message.from_user.first_name
+#     u_lastname = message.from_user.last_name
+#     u_username = message.from_user.username
+#     print(u_id, u_name, u_lastname, u_username)
+#     bot.reply_to(message, f"Welcome, user{u_id}, {u_name}, {u_lastname}, {u_username}")
 
 
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
-    print(message.text)
-    print(message.from_user.id)
+    log_file = open(conf.LOG_FILE, 'a+', encoding="UTF-8")
+    log_string = f'{datetime.now()}, - Запрос от пользователя {message.from_user.id}  {message.text} Записан в БД \n'
+    log_file.write(log_string)
+    log_file.close()
+
     # search_message_in_db(message)
-    bot.reply_to(message, f'{search_message_in_db(message)}')
+    # bot.reply_to(message, f'{store_message_in_db(message)}')
 
 
 bot.infinity_polling()

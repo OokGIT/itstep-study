f7702299a3a24d5441dc7befb0040d2ac9934872
point48 <aeroflot.ua@gmail.com>
2021-11-17 17:06:19 +0200

    Some structural fixes

--- Flask/FirstHomeWork/views.py
+++ Flask/FirstHomeWork/views.py
@@ -16,6 +16,8 @@ def index():
         {'name': 'Intel Core i5-10400', 'freq': '2.9GHz', 'cache': '12MB', 'price': '5359'},
         {'name': 'Intel Core i7-11700K', 'freq': '3.6Hz', 'cache': '16MB', 'price': '12011'},
         {'name': 'Intel Core i5-10400F', 'freq': '2.9GHz', 'cache': '12MB', 'price': '4799'},
+        {'name': 'Intel Core i9-12900K', 'freq': '3.2GHz', 'cache': '30MB', 'price': '21299'},
+        {'name': 'AMD Ryzen 5 5600X', 'freq': '3.7GHz', 'cache': '32MB', 'price': '9365'},
     ]
     return render_template('index.html', name='Процессоры', result=res)
 
